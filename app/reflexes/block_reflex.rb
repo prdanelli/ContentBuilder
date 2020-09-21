5ac0ad3d8efb13c0bdc6a542ea81c696dc2d65ab
Paul Whitehead <prwhitehead@gmail.com>
2020-09-21 16:44:03 +0100

    Get blocks added and deleted with reflex

--- app/reflexes/block_reflex.rb
+++ app/reflexes/block_reflex.rb
@@ -1,12 +1,18 @@
 # frozen_string_literal: true
 
 class BlockReflex < ApplicationReflex
-  before_reflex :build_post, only: %w(create)
+  before_reflex :build_post, only: %w(create destroy)
 
   def create
     block = @post.blocks.build(permitted_params)
 
     block.save
+  end
+
+  def destroy
+    @post.blocks.find(block_id).destroy
+
+    @post.reload
   end
 
   protected
@@ -17,5 +23,9 @@ class BlockReflex < ApplicationReflex
 
   def build_post
     @post ||= Post.find(element.dataset.dig(:post_id).to_i)
+  end
+
+  def block_id
+    element.dataset.dig(:id).to_i
   end
 end
